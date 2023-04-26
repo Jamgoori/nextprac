6f34b489a42fb7ad66d9e9fce21a0375d7b8df2e
Jamgoori <lks2354@naver.com>
2023-04-26 20:49:56 +0900

    공통부분 분리

--- my-app/src/app/products/[slug]/page.tsx
+++ my-app/src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { getProduct, getProducts } from "@/service/products";
 import Link from "next/link";
 
 type Props = {
@@ -6,20 +7,26 @@ type Props = {
   };
 };
 
-const products = ["shirt", "pants", "skirt", "shoes"];
-export default function page({ params }: Props) {
-  return (
-    <div>
-      <h1>제품 소개 페이지~!</h1>
-      <div>{params.slug} 레시피!!</div>
-      <div>{params.slug} 무봤나 ㅋㅋ </div>
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            <Link href={`products/${product}`}>{product}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+export function generateMetadata({ params }: Props) {
+  return {
+    title: `제품의 이름: ${params.slug}`,
+  };
+}
+
+export default function page({ params: { slug } }: Props) {
+  const product = getProduct(slug);
+
+  if (!product) {
+    return <div>Not Found 404</div>;
+  }
+  //서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌
+  return <h1>{slug}제품 소개 페이지~!</h1>;
+}
+
+export function generateStaticParams() {
+  //모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄것 (SSG)
+  const products = getProducts();
+  return products.map((product) => ({
+    slug: product,
+  }));
 }
---
+++ my-app/src/service/products.ts
@@ -0,0 +1,7 @@
+export function getProducts() {
+  return ["shirt", "pants", "skirt", "shoes", "socks"];
+}
+
+export function getProduct(id: string) {
+  return "shirt";
+}
