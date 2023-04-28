3c5f29fa93fe98e67bddb372e1d0cafa1569b5d8
Jamgoori <lks2354@naver.com>
2023-04-28 23:42:49 +0900

    data파일 사용해보기

--- my-app/src/app/products/[slug]/page.tsx
+++ my-app/src/app/products/[slug]/page.tsx
@@ -13,20 +13,20 @@ export function generateMetadata({ params }: Props) {
   };
 }
 
-export default function page({ params: { slug } }: Props) {
-  const product = getProduct(slug);
+export default async function Productpage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
 
   if (!product) {
     return <div>Not Found 404</div>;
   }
   //서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{slug}제품 소개 페이지~!</h1>;
+  return <h1>{product.name} 제품 소개 페이지~!</h1>;
 }
 
-export function generateStaticParams() {
+export async function generateStaticParams() {
   //모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄것 (SSG)
-  const products = getProducts();
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,16 +1,16 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 
-export default function page() {
+export default async function page() {
   //서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
-  const products = getProducts();
+  const products = await getProducts();
   return (
     <>
       <div>제품 소개 페이지 입니다.</div>
       <ul>
         {products.map((product, index) => (
           <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+            <Link href={`/products/${product.id}`}>{product.name}</Link>
           </li>
         ))}
       </ul>
